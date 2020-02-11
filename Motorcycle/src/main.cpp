f0343cbe7937b57600f061f2c85846dce40c208a
Aidan <25093511+msbundles@users.noreply.github.com>
2020-02-11 17:27:16 -0600

    Spin up and down

--- Motorcycle/src/main.cpp
+++ Motorcycle/src/main.cpp
@@ -15,8 +15,8 @@
 using namespace vex;
 motor RTR = motor(PORT1, ratio18_1, false);
 motor RTL = motor(PORT2, ratio18_1, false);
-motor RBL = motor(PORT3, ratio18_1, false);
-motor RBR = motor(PORT4, ratio18_1, false);
+motor RBL = motor(PORT3, ratio18_1, true);
+motor RBR = motor(PORT4, ratio18_1, true);
 motor LTR = motor(PORT5, ratio18_1, false);
 motor LTL = motor(PORT6, ratio18_1, false);
 motor LBL = motor(PORT7, ratio18_1, false);
@@ -35,18 +35,55 @@ int main() {
   LBL.setStopping(brakeType::coast);
   LBR.setStopping(brakeType::coast);
   steer.setStopping(brakeType::hold);
+  Controller1.Screen.clearScreen();
   Controller1.Screen.print("Vroom Vroom");
   Brain.Screen.print("Vroom Vroom");
-  while(1){
-    RTR.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
-    RTL.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
-    RBR.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
-    RBL.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
-    LTR.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
-    LTL.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
-    LBR.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
-    LBL.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
-    steer.spin(directionType::fwd,Controller1.Axis4.value(), velocityUnits::pct);
-    }
+
   
+  while(1){
+    if(Controller1.ButtonX.pressing()){
+      for(int i=0; i<100;i++){
+        RTR.spin(directionType::fwd,i, velocityUnits::pct);
+        RTL.spin(directionType::fwd,i, velocityUnits::pct);
+        RBR.spin(directionType::fwd,i, velocityUnits::pct);
+        RBL.spin(directionType::fwd,i, velocityUnits::pct);
+        LTR.spin(directionType::fwd,i, velocityUnits::pct);
+        LTL.spin(directionType::fwd,i, velocityUnits::pct);
+        LBR.spin(directionType::fwd,i, velocityUnits::pct);
+        LBL.spin(directionType::fwd,i, velocityUnits::pct);
+        task::sleep(200);
+      }
+      Controller1.Screen.print("Spinup Done");
+      task::sleep(500);
+      Controller1.Screen.clearScreen();
+    }
+    else if(Controller1.ButtonB.pressing()){
+      for(int i=0; i>0;i--){
+        RTR.spin(directionType::fwd,i, velocityUnits::pct);
+        RTL.spin(directionType::fwd,i, velocityUnits::pct);
+        RBR.spin(directionType::fwd,i, velocityUnits::pct);
+        RBL.spin(directionType::fwd,i, velocityUnits::pct);
+        LTR.spin(directionType::fwd,i, velocityUnits::pct);
+        LTL.spin(directionType::fwd,i, velocityUnits::pct);
+        LBR.spin(directionType::fwd,i, velocityUnits::pct);
+        LBL.spin(directionType::fwd,i, velocityUnits::pct);
+        task::sleep(500);
+      }
+      Controller1.Screen.print("Spindown Done");
+      task::sleep(500);
+      Controller1.Screen.clearScreen();
+
+    }
+    else{
+      RTR.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
+      RTL.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
+      RBR.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
+      RBL.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
+      LTR.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
+      LTL.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
+      LBR.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
+      LBL.spin(directionType::fwd,Controller1.Axis2.value(), velocityUnits::pct);
+      steer.spin(directionType::fwd,Controller1.Axis4.value(), velocityUnits::pct);
+    }
+  }
 }
